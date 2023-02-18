b560ead3dc844289a426dc5c8402f2c54681405f
Ali Yassine <aliyassine_2000@hotmail.com>
2023-02-18 19:59:40 +0100

    add least squares function

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -1,5 +1,5 @@
-from .least_squares import least_squares
 from .scipy_interpolation import interpolate_data
 from .interpolation import global_interp, piecewise_interp
+from .least_squares import least_squares
 
 __all__ = ['interpolate_data','global_interp', 'piecewise_interp','least_squares']
--- algorithms/least_squares.py
+++ algorithms/least_squares.py
@@ -1,12 +1,28 @@
 import numpy as np
+import pandas as pd
 
-def least_squares(x, y):
-    x = np.array(x)
-    y = np.array(y)
-    x_mean = np.mean(x)
-    y_mean = np.mean(y)
-    numerator = np.sum((x - x_mean) * (y - y_mean))
-    denominator = np.sum((x - x_mean) ** 2)
-    beta_1 = numerator / denominator
-    beta_0 = y_mean - beta_1 * x_mean
-    return beta_0, beta_1
+# Define a function to fill in missing data using the pseudo-inverse
+def least_squares(df,colname):
+    # Split the DataFrame into two parts - one with missing values, and one without
+    df_missing = df[df.isna().any(axis=1)]
+    df_complete = df.dropna()
+    
+    # Extract the values of the complete part of the DataFrame
+    X = df_complete.index.values
+    y = df_complete[colname].values
+    
+    # Compute the pseudo-inverse of X
+    X_pinv = np.linalg.pinv(X.reshape(-1, 1))
+    
+    # Use the pseudo-inverse to estimate the missing values
+    y_pred = X_pinv.dot(y)
+    
+    # Add the estimated values to the DataFrame
+    df_missing[colname] = df_missing.index.values.reshape(-1, 1).dot(y_pred)
+    
+    # Combine the complete and estimated parts of the DataFrame
+    df_result = pd.concat([df_complete, df_missing], axis=0)
+    df_result = df_result.sort_index()
+    return df_result
+
+
